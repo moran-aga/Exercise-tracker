23032985a38cf217c5aefd58d1075b547554c87c
moran-aga <77380388+moran-aga@users.noreply.github.com>
2021-03-12 12:40:01 +0200

    response equal but test fails

--- server.js
+++ server.js
@@ -29,16 +29,25 @@ const userSchema = new mongoose.Schema({
 });
 const User = mongoose.model("User", userSchema);
 
+const exerciseSchema = new mongoose.Schema({
+ userId: mongoose.Types.ObjectId,
+ description: String,
+ duration: Number,
+ date: {
+  type: Date,
+  default: new Date(),
+ },
+});
+const Exercise = mongoose.model("Exercise", exerciseSchema);
+
 app.get("/", (req, res) => {
  res.sendFile(__dirname + "/views/index.html");
 });
 
 app.post("/api/exercise/new-user", (req, res) => {
  const body = req.body;
- console.log(body);
  const newUser = new User({ username: body.username });
  newUser.save();
- api / exercise / users;
  res.send(newUser);
 });
 
@@ -50,12 +59,52 @@ app.get("/api/exercise/users", (req, res) => {
 
 app.post("/api/exercise/add", (req, res) => {
  const body = req.body;
- console.log(body);
- const newUser = new User({ username: body.username });
- newUser.save();
- api / exercise / users;
- res.send(newUser);
+ let finalDate = body.date;
+ if (body.date === "") {
+  let date = new Date();
+  finalDate = `${date.getFullYear()} ${date.getMonth() + 1} ${
+   date.getDate() + 1
+  }`;
+ }
+ const newExercise = new Exercise({
+  id: body.userId,
+  description: body.description,
+  duration: body.duration,
+  date: finalDate,
+ });
+ newExercise.save();
+ User.findById(body.userId)
+  .then((user) => {
+   let data = {
+    username: user.username,
+    description: newExercise.description,
+    duration: newExercise.duration,
+    _id: user.id,
+    date: dateFormat(finalDate),
+   };
+   res.json(data);
+  })
+  .catch((err) => {
+   res.send(err);
+  });
 });
+
+// app.get("/api/exercise/log?{userId}[&from][&to][&limit]", (req, res) => {
+//  const id = req.query.userId;
+//  console.log(req.query);
+// });
+
+function dateFormat(dateString) {
+ let date = new Date(dateString);
+ let year = date.getFullYear();
+ let day = date.getDate();
+ let month = new Intl.DateTimeFormat("en-US", { month: "short" }).format(date);
+ let weekday = new Intl.DateTimeFormat("en-US", {
+  weekday: "short",
+ }).format(date);
+ return `${weekday} ${month} ${day} ${year}`;
+}
+
 const listener = app.listen(process.env.PORT || 3000, () => {
  console.log("Your app is listening on port " + listener.address().port);
 });
